95e0d481fcb928b1d3d40b7c7afd540f60531b17
Soumalya Karak <soumalyakarak2@gmail.com>
2026-07-08 19:43:00 +0530

    cors setup and utils update

--- server/index.js
+++ server/index.js
@@ -1,25 +1,31 @@
 import express from "express";
 import http from "http";
-// import cors from "cors";
+import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 
+import { coursesRouter } from "./routes/course.routes.js";
+import { youtubeRouter } from "./routes/youtube.routes.js";
+
 
 const app = express();
-// const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:3000";
+const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:5173";
 /* ---------------- middleware ---------------- */
 // app.use(cookieParser());
-// app.use(
-//   cors({
-//     origin: CLIENT_URL,
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: CLIENT_URL,
+    credentials: true,
+  })
+);
 app.use(express.json());    
 /* ---------------- routes ---------------- */
-app.get("/", (req, res) => {
-    res.send("Server is running!");
-});
+app.get("/", (req,res) => {
+    res.json({ message: "Welcome to Text-To-Learn!" });
+})
+app.use("/api/courses", coursesRouter);
+app.use("/api/youtube", youtubeRouter);
+
 
 /* ---------------- error handler ---------------- */
 
