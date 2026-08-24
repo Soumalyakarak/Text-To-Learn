0a6c2b2205bf01a5cc19eedabbca960b0a3a2cdf
Soumalya Karak <soumalyakarak2@gmail.com>
2026-08-24 19:08:15 +0530

    audio generation route is added

--- server/routes/lesson.routes.js
+++ server/routes/lesson.routes.js
@@ -1,6 +1,8 @@
 import express from "express";
-import { getOrGenerateLesson } from "../controllers/lesson.controller.js";
+import { getLesson, getLessonAudio} from "../controllers/lesson.controller.js";
 
 export const lessonRouter = express.Router();
 
-lessonRouter.get("/:courseId/module/:moduleIndex/lesson/:lessonIndex", getOrGenerateLesson);
+lessonRouter.post("/audio/generate",getLessonAudio);
+
+lessonRouter.get("/:courseId/module/:moduleIndex/lesson/:lessonIndex", getLesson);
