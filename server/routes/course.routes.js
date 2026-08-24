298af74bec4d2dd908766b471345538c2ecf737d
Soumalya Karak <soumalyakarak2@gmail.com>
2026-08-24 19:06:54 +0530

    middleware is added for protected route

--- server/routes/course.routes.js
+++ server/routes/course.routes.js
@@ -3,14 +3,21 @@ import {
   getCourses, 
   getCourse, 
   generateCourse, 
-  getLesson, 
-  markLessonDone 
+  markLessonDone, 
+  deleteCourse
 } from "../controllers/course.controller.js";
+import { authenticateUser } from "../middlewares/auth.middleware.js";
+import { getLesson} from "../controllers/lesson.controller.js";
 
 export const coursesRouter = Router();
 
-coursesRouter.get("/", getCourses);
-coursesRouter.get("/:id", getCourse);
-coursesRouter.post("/generate", generateCourse);
-coursesRouter.get("/:courseId/lessons/:moduleIndex/:lessonIndex", getLesson);
+coursesRouter.use(authenticateUser);
+
+coursesRouter.get("/:courseId/module/:moduleIndex/lesson/:lessonIndex", getLesson);
 coursesRouter.patch("/:courseId/lessons/:moduleIndex/:lessonIndex/done", markLessonDone);
+
+coursesRouter.get("/", getCourses);
+coursesRouter.post("/generate", generateCourse);
+
+coursesRouter.get("/:id", getCourse);
+coursesRouter.delete("/:id", deleteCourse);
